caaed19510edcb76436f97e5d88cd324b5d3e766
Nazeer636 <najeershaik4258@gmail.com>
2024-07-20 16:17:01 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+dictionary={'a':1,'b':2,'c':3}
+n=int(input("enter n0.of elements:"))
+for i in range(n):
+    name=input("enter key:")
+    
+for i in range (n):
+    t=int(input("enter value"))
+    
+print("dictionary=",dictionary)
